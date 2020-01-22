49ba40beb36baf0f98fd0cd93e923a073b051072
BarbaraSmok <barb@kpimedia.com>
2020-01-21 21:41:30 -0300

    js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,11 @@
-const randomuserUrl = 'https://randomuser.me/api/';
-const card = document.querySelector('.card');
-const employeesList = document.getElementById('employees');
+//Global variables
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 
 //Fetch Functions
 function fetchData(url) {
